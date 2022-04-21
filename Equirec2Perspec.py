753ec24d7aa471ecae110fe25a773a5594d916d2
Joao Carreira <joao.carreira@robustbutton.pt>
2022-04-21 22:18:35 +0100

    My tests

--- Equirec2Perspec.py
+++ Equirec2Perspec.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import cv2
-import cupy as np
+import numpy as np
 import time
 
 
@@ -17,7 +17,10 @@ def xyz2lonlat(xyz):
     asin = np.arcsin
 
     norm = np.linalg.norm(xyz, axis=-1, keepdims=True)
+#    print( norm[0] )
     xyz_norm = xyz / norm
+#    print( xyz_norm[0] )
+    
     x = xyz_norm[..., 0:1]
     y = xyz_norm[..., 1:2]
     z = xyz_norm[..., 2:]
@@ -61,13 +64,15 @@ class Equirectangular:
         cy = (height - 1) / 2.0
 
         K = np.array([
-                [f1.get(), 0, cx],
-                [0, f2.get(), cy],
+                [f1, 0, cx],
+                [0, f2, cy],
                 [0, 0,  1],
             ], np.float32 )
+ #       print( K )
 
         K_inv = np.linalg.inv(K)
 #        print(time.time() - start)
+#        print( K_inv )
 
         x = np.arange(width)
         y = np.arange(height)
@@ -75,23 +80,40 @@ class Equirectangular:
         z = np.ones_like(x)
         xyz = np.concatenate([x[..., None], y[..., None], z[..., None]], axis=-1)
 #        print("Passo  7: %.4f"%(time.time() - start))
+#        print( xyz[0, 0] )
 
+        print(K_inv.T)
         xyz = xyz @ K_inv.T
+#        print( xyz[0, 0] )
+
+
 #        print("Passo  8: %.4f"%(time.time() - start))
 
         y_axis = np.array([0.0, 1.0, 0.0], np.float32)
         x_axis = np.array([1.0, 0.0, 0.0], np.float32)
-        R1, t = cv2.Rodrigues((y_axis * np.radians(THETA)).get())
-        R2, t = cv2.Rodrigues( (np.dot( np.asarray(R1), x_axis) * np.radians(PHI)).get()  )
+        R1, t = cv2.Rodrigues((y_axis * np.radians(THETA)))
+        print(R1)
+        
+        print( np.dot( np.asarray(R1), x_axis) )
+        print( (np.dot( np.asarray(R1), x_axis) * np.radians(PHI))  )
+
+        R2, t = cv2.Rodrigues( (np.dot( np.asarray(R1), x_axis) * np.radians(PHI)) )
+        print(R2)
         R = R2 @ R1
+        print(R.T)
+
 #        print("Passo  9: %.4f"%(time.time() - start))
+
+#        print( xyz[0, 0] )
         xyz = xyz @ np.asarray(R.T)
+        print( xyz[0, 0] )
+
 #        print("Passo 10: %.4f"%(time.time() - start))
         lonlat = xyz2lonlat(xyz)
 #        print("Passo 11: %.4f"%(time.time() - start))
 
         XY = lonlat2XY(lonlat, shape=self._img.shape).astype(np.float32)
-        persp = cv2.remap(self._img, (XY[..., 0]).get(), (XY[..., 1]).get(), cv2.INTER_LINEAR, borderMode=cv2.BORDER_WRAP)
+        persp = cv2.remap(self._img, (XY[..., 0]), (XY[..., 1]), cv2.INTER_LINEAR, borderMode=cv2.BORDER_WRAP)
 #        print(time.time() - start)
 
         return persp
@@ -103,14 +125,11 @@ class Equirectangular:
 
 e = Equirectangular("src/image.jpg")
 
-h = e._height//5
-w = e._width//5
-
-r2 = 5
-
+#h = e._height//5
+#w = e._width//5
+#r2 = 5
 #e.img[ 0:e._height, 0:e._width//r2 ] = [0, 255, 0]
 #e.img[ 0:e._height, e._width - e._width//r2: e._width ] = [0, 255, 0]
-
 # for i in range( e._height ):
 #     for j in range( e._width//r2):
 #         e.img[i][j] = [0, 255, 0]
@@ -123,12 +142,13 @@ r2 = 5
 
 start = time.time()
 
-#for i in range(10):
-#    start = time.time()
-#    img = e.GetPerspective( 85, 0, 0, 720, 1024)
-#    print(time.time() - start)
-#    print("Fim")
+for i in range(10):
+    start = time.time()
+    img = e.GetPerspective( 85, 90, 0, 720, 1280)
+    print(time.time() - start)
+    print("Fim")
 
+'''
 #print(time.time() - start)
 #cv2.imshow('image', img)
 #waitClose()
@@ -143,7 +163,7 @@ frameNr = 0
     
 while angle < 90:
     framestart = time.time()
-    img = e.GetPerspective( 85, angle, 0, 720, 1024)
+    img = e.GetPerspective( 85, angle, 0, 18, 25)
     #img2 = cv2.cvtColor( img, cv2.COLOR_BAYER_BG2RGB )
     writer.writeFrame(img)
 
@@ -157,9 +177,9 @@ while angle < 90:
     frameNr = frameNr + 1
     print( "frameNr %06d Angle %.4f  Delay %d " % (frameNr, angle,delay) )
 
-
 writer.close()
+'''
 
+cv2.imshow('image', img)
+waitClose()
 
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
